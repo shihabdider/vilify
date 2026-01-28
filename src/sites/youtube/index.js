a4af919f8665bb2a0f8e5af3006d61a899fd5ea6
shihabdider <shihabdider@gmail.com>
2026-01-28 15:12:04 -0500

    feat(youtube): wire up player and commands in config
    - Import and export player controls
    - Import and export command generators
    - Add getCommands, getKeySequences, getSingleKeyActions to config
    - Re-export all modules for direct imports

--- src/sites/youtube/index.js
+++ src/sites/youtube/index.js
@@ -1,7 +1,8 @@
 // YouTube site configuration
-// TODO: Implement YouTube site config
 
 import { getVideos, getChapters, getComments, getYouTubePageType, getVideoContext, getDescription } from './scraper.js';
+import { getYouTubeCommands, getYouTubeKeySequences, getYouTubeSingleKeyActions } from './commands.js';
+import * as player from './player.js';
 
 export const youtubeConfig = {
   name: 'youtube',
@@ -25,12 +26,31 @@ export const youtubeConfig = {
     description: getDescription,
   },
   
-  // Commands - TODO: implement
-  commands: [],
+  // Commands
+  getCommands: getYouTubeCommands,
+  getKeySequences: getYouTubeKeySequences,
+  getSingleKeyActions: getYouTubeSingleKeyActions,
   
-  // Player controls - TODO: implement
-  player: {},
+  // Player controls
+  player: {
+    getVideo: player.getVideo,
+    togglePlayPause: player.togglePlayPause,
+    seekRelative: player.seekRelative,
+    setPlaybackRate: player.setPlaybackRate,
+    toggleMute: player.toggleMute,
+    toggleFullscreen: player.toggleFullscreen,
+    toggleTheaterMode: player.toggleTheaterMode,
+    toggleCaptions: player.toggleCaptions,
+    seekToChapter: player.seekToChapter,
+    seekToPercent: player.seekToPercent,
+    applyDefaultVideoSettings: player.applyDefaultVideoSettings,
+  },
   
   // Page renderers - TODO: implement
   renderers: {},
 };
+
+// Re-export for direct imports
+export * from './scraper.js';
+export * from './commands.js';
+export * from './player.js';
